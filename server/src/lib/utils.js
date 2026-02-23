462af30f5c8f63bfa73dfb8de7924573210bb27c
Gagan Shukla <106883823+Gaganshukla01@users.noreply.github.com>
2026-02-23 19:10:02 +0530

    Update utils.js

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -1,17 +1,15 @@
-import { strict } from "assert"
 import jwt from "jsonwebtoken"
 
-export const genrateToken=(userId,res)=>{
-    const sKey=process.env.JWT_SECRET
-    const token= jwt.sign({userId},sKey,{
-        expiresIn:'7d'
+export const genrateToken = (userId, res) => {
+    const sKey = process.env.JWT_SECRET
+    const token = jwt.sign({ userId }, sKey, {
+        expiresIn: '7d'
     })
-
-    res.cookie("jwt",token,{
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
-        samSite:strict,
-        secure:process.env.NODE_ENV!=="development"
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "none",  
+        secure: true       
     })
-     return token;
+    return token;
 }
